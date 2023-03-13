1fa248777f5f5156a24232ffda120c537097751b
Fatih Kalemci <fatihkalemci96@gmail.com>
2023-03-13 22:18:51 +0300

    Add game itself

--- script.js
+++ script.js
@@ -15,24 +15,61 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    return "It's a tie! What a bummer!";
+    return "tie";
   }
 
+  // win lose case is on players perspective
+  let result
   switch (playerSelection) {
     case "rock":
-      return computerSelection === "scissors" ? "You win! Rock beats Scissors." : "You lost! Paper beats Rock.";
+      result = computerSelection === "scissors" ? "win" : "lose";
       break;
     case "paper":
-      return computerSelection === "rock" ? "You win! Paper beats Rock." : "You lost! Scissors beats Paper.";
+      result = computerSelection === "rock" ? "win" : "lose";
       break;
     case "scissors":
-      return computerSelection === "paper" ? "You win! Scissors beats Paper." : "You lost! Paper beats Rock.";
+      result = computerSelection === "paper" ? "win" : "lose";
       break;
     default:
       break;
   }
+
+  return result
 }
 
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    console.log(`Player ${playerScore} - ${computerScore} Computer`)
+    let playerSelection = prompt("rock, paper or scissors?");
+    let computerSelection = getComputerChoice();
+    let round = playRound(playerSelection, computerSelection);
+    console.log(`Player chose: ${playerSelection} | Computer chose: ${computerSelection}`)
+    if (round === "win") {
+      console.log(`Point to player! ${playerSelection} beats ${computerSelection}`);
+      playerScore++;
+    } else if (round === "lose") {
+      console.log(`Point to computer! ${computerSelection} beats ${playerSelection}`);
+      computerScore++;
+    }
+
+  }
+
+  if (playerScore > computerScore) {
+    console.log(`Player: ${playerScore} - Computer: ${computerScore} | Player WINS`);
+  } else if (playerScore < computerScore) {
+    console.log(`Player: ${playerScore} - Computer: ${computerScore} | Computer WINS! YOU LOSE!`);
+  } else {
+    console.log("It's a DRAW!")
+  }
+}
+
+/*
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+*/
+
+game()
