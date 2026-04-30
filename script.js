2f4e8bc3c33f0c223ca9be64818176d971ecb3a6
Fatih Kalemci <fatihkalemci96@gmail.com>
2026-05-01 00:02:50 +0300

    Finish UI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3)
   let choice
@@ -16,15 +19,9 @@ function getHumanChoice() {
   return choice
 }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-  if (playerSelection === computerSelection) {
-    return "tie";
-  }
+function compareHands(playerSelection, computerSelection){
 
-  // win lose case is on players perspective
-  let result
+  if (playerSelection === computerSelection) return "tie";
   switch (playerSelection) {
     case "rock":
       result = computerSelection === "scissors" ? "win" : "lose";
@@ -36,45 +33,57 @@ function playRound(playerSelection, computerSelection) {
       result = computerSelection === "paper" ? "win" : "lose";
       break;
     default:
+      result = "tie"
       break;
   }
 
-  return result
+  return result;
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+function playRound(playerSelection) {
 
-  for (let i = 0; i < 5; i++) {
-    console.log(`Player ${playerScore} - ${computerScore} Computer`)
-    let playerSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    let round = playRound(playerSelection, computerSelection);
-    console.log(`Player chose: ${playerSelection} | Computer chose: ${computerSelection}`)
-    if (round === "win") {
-      console.log(`Point to player! ${playerSelection} beats ${computerSelection}`);
-      playerScore++;
-    } else if (round === "lose") {
-      console.log(`Point to computer! ${computerSelection} beats ${playerSelection}`);
-      computerScore++;
-    }
-
+  if (computerScore >= 5 || playerScore >= 5) {
+    winner = computerScore > playerScore ? "computer" : "player"
+    roundP.textContent =`${winner.toUpperCase()} WON!`;
+    return 0
   }
 
-  if (playerScore > computerScore) {
-    console.log(`Player: ${playerScore} - Computer: ${computerScore} | Player WINS`);
-  } else if (playerScore < computerScore) {
-    console.log(`Player: ${playerScore} - Computer: ${computerScore} | Computer WINS! YOU LOSE!`);
+  let computerSelection = getComputerChoice().toLowerCase();
+
+  let result = compareHands(playerSelection, computerSelection);
+
+  if (result === "win") {
+    roundP.textContent =`Point to player! ${playerSelection} beats ${computerSelection}`;
+    playerScore++;
+  } else if (result === "lose") {
+    roundP.textContent =`Point to computer! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
   } else {
-    console.log("It's a DRAW!")
+    roundP.textContent =`No Points! ${computerSelection} and ${playerSelection} ends in a draw`;
   }
+
+  scoreP.textContent = `Player ${playerScore} - ${computerScore} Computer`
+
 }
 
-/*
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-*/
+function game(){
+  rockButton = document.querySelector("#rock");
+  paperButton = document.querySelector("#paper");
+  scissorsButton = document.querySelector("#scissors");
+  rockButton.addEventListener('click', () => playRound("rock"))
+  paperButton.addEventListener('click', () => playRound("paper"))
+  scissorsButton.addEventListener('click', () => playRound("scissors"))
+
+
+  resultDiv = document.querySelector(".results");
+  scoreP = document.createElement("p");
+  roundP = document.createElement("p");
+
+  resultDiv.appendChild(scoreP)
+  resultDiv.appendChild(roundP)
+
+  scoreP.textContent = `Player ${playerScore} - ${computerScore} Computer`
+  roundP.textContent =`GAME START`;
+}
 
 game()
